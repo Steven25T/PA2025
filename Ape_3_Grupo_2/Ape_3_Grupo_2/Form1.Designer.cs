d5d476e19d6f285015f32bac02ee7dc0182701dd
Steven25T <stevengiovanny2000@gmail.com>
2025-04-24 18:03:38 -0500

    Add files via upload

---
+++ Ape_3_Grupo_2/Ape_3_Grupo_2/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Ape_3_Grupo_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.txtCedula = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.dgvResultados = new System.Windows.Forms.DataGridView();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.btnEditar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(338, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Formulario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(54, 108);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(217, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cedula de identificacion:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(54, 170);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nombre:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(56, 231);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Apellido:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(54, 301);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Telefono:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(58, 375);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(189, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Fecha de nacimeinto:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(64, 435);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(173, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Correo Electronico:";
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.Location = new System.Drawing.Point(278, 370);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(137, 27);
+            this.dtpFecha.TabIndex = 7;
+            // 
+            // txtCedula
+            // 
+            this.txtCedula.Location = new System.Drawing.Point(278, 103);
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.Size = new System.Drawing.Size(137, 27);
+            this.txtCedula.TabIndex = 8;
+            this.txtCedula.TextChanged += new System.EventHandler(this.txtCedula_TextChanged);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(278, 163);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(137, 27);
+            this.txtNombre.TabIndex = 9;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(278, 223);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(137, 27);
+            this.txtApellido.TabIndex = 10;
+            this.txtApellido.TextChanged += new System.EventHandler(this.txtApellido_TextChanged);
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(278, 293);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(137, 27);
+            this.txtTelefono.TabIndex = 11;
+            this.txtTelefono.TextChanged += new System.EventHandler(this.txtTelefono_TextChanged);
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(278, 432);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(137, 27);
+            this.txtCorreo.TabIndex = 12;
+            this.txtCorreo.TextChanged += new System.EventHandler(this.txtCorreo_TextChanged);
+            // 
+            // dgvResultados
+            // 
+            this.dgvResultados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResultados.Location = new System.Drawing.Point(507, 103);
+            this.dgvResultados.Name = "dgvResultados";
+            this.dgvResultados.RowHeadersWidth = 51;
+            this.dgvResultados.RowTemplate.Height = 24;
+            this.dgvResultados.Size = new System.Drawing.Size(486, 147);
+            this.dgvResultados.TabIndex = 13;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(600, 293);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(103, 36);
+            this.btnGuardar.TabIndex = 14;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.Location = new System.Drawing.Point(773, 293);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(125, 36);
+            this.btnEditar.TabIndex = 15;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 562);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.dgvResultados);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCedula);
+            this.Controls.Add(this.dtpFecha);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ImeMode = System.Windows.Forms.ImeMode.Hiragana;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "APE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.TextBox txtCedula;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.DataGridView dgvResultados;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button btnEditar;
+    }
+}
+
---
+++ Ape_3_Grupo_2/Ape_3_Grupo_2/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ape_3_Grupo_2
+{
+    public partial class Form1 : Form
+    {
+        private DataGridViewRow filaEditando = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            // Recoger datos del formulario
+            string cedula = txtCedula.Text;
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            string telefono = txtTelefono.Text;
+            string correo = txtCorreo.Text;
+            DateTime fechaNacimiento = dtpFecha.Value;
+
+            // Validaciones básicas
+            if (string.IsNullOrWhiteSpace(cedula) ||
+                string.IsNullOrWhiteSpace(nombre) ||
+                string.IsNullOrWhiteSpace(apellido))
+            {
+                MessageBox.Show("Por favor, complete los campos obligatorios.");
+                return;
+            }
+
+            // Agregar datos al DataGridView
+            dgvResultados.Rows.Add(cedula, nombre, apellido, telefono, fechaNacimiento.ToShortDateString(), correo);
+
+            // Limpiar campos luego de guardar
+            LimpiarCampos();
+
+        }
+        private void LimpiarCampos()
+        {
+            txtCedula.Clear();
+            txtNombre.Clear();
+            txtApellido.Clear();
+            txtTelefono.Clear();
+            txtCorreo.Clear();
+            dtpFecha.Value = DateTime.Now;
+            filaEditando = null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvResultados.ColumnCount = 6;
+            dgvResultados.Columns[0].Name = "Cédula";
+            dgvResultados.Columns[1].Name = "Nombre";
+            dgvResultados.Columns[2].Name = "Apellido";
+            dgvResultados.Columns[3].Name = "Teléfono";
+            dgvResultados.Columns[4].Name = "Fecha de Nacimiento";
+            dgvResultados.Columns[5].Name = "Correo Electrónico";
+        }
+
+        private void txtCedula_TextChanged(object sender, EventArgs e)
+        {
+            // Eliminar caracteres que no sean dígitos
+            string textoFiltrado = new string(txtCedula.Text.Where(char.IsDigit).ToArray());
+
+            // Limitar a 10 dígitos
+            if (textoFiltrado.Length > 10)
+                textoFiltrado = textoFiltrado.Substring(0, 10);
+
+            // Si hubo cambios, actualiza el texto del textbox
+            if (txtCedula.Text != textoFiltrado)
+            {
+                int pos = txtCedula.SelectionStart - 1;
+                txtCedula.Text = textoFiltrado;
+                txtCedula.SelectionStart = Math.Max(0, pos);
+            }
+
+            // Mostrar error si no hay 10 dígitos (opcional)
+            if (txtCedula.Text.Length < 10)
+            {
+                errorProvider1.SetError(txtCedula, "La cédula debe tener exactamente 10 números.");
+            }
+            else
+            {
+                errorProvider1.SetError(txtCedula, "");
+            }
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            string textoFiltrado = new string(txtNombre.Text.Where(c => char.IsLetter(c) || c == ' ').ToArray());
+
+            // Convertir a título (solo la primera letra de cada palabra será mayúscula)
+            textoFiltrado = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(textoFiltrado.ToLower());
+
+            // Si el texto filtrado ha cambiado, actualizar el texto en el TextBox
+            if (txtNombre.Text != textoFiltrado)
+            {
+                txtNombre.Text = textoFiltrado;
+                // Mantener la posición del cursor
+                txtNombre.SelectionStart = txtNombre.Text.Length;
+            }
+
+        }
+
+        private void txtApellido_TextChanged(object sender, EventArgs e)
+        {
+            string textoFiltrado = new string(txtApellido.Text.Where(c => char.IsLetter(c) || c == ' ').ToArray());
+
+            // Convertir a título (solo la primera letra de cada palabra será mayúscula)
+            textoFiltrado = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(textoFiltrado.ToLower());
+
+            // Si el texto filtrado ha cambiado, actualizar el texto en el TextBox
+            if (txtApellido.Text != textoFiltrado)
+            {
+                txtApellido.Text = textoFiltrado;
+                // Mantener la posición del cursor
+                txtApellido.SelectionStart = txtNombre.Text.Length;
+            }
+        }
+
+        private void txtTelefono_TextChanged(object sender, EventArgs e)
+        {
+            // Eliminar caracteres que no sean dígitos
+            string textoFiltrado = new string(txtTelefono.Text.Where(char.IsDigit).ToArray());
+
+            // Forzar que comience con 0
+            if (!string.IsNullOrEmpty(textoFiltrado) && textoFiltrado[0] != '0')
+            {
+                textoFiltrado = "0" + textoFiltrado;
+            }
+
+            // Limitar a 10 dígitos
+            if (textoFiltrado.Length > 10)
+                textoFiltrado = textoFiltrado.Substring(0, 10);
+
+            // Si hubo cambios, actualiza el texto del textbox
+            if (txtTelefono.Text != textoFiltrado)
+            {
+                int pos = txtTelefono.SelectionStart;
+                txtTelefono.Text = textoFiltrado;
+                txtTelefono.SelectionStart = Math.Min(txtTelefono.Text.Length, pos);
+            }
+
+            // Validación final
+            if (txtTelefono.Text.Length < 10)
+            {
+                errorProvider1.SetError(txtTelefono, "El teléfono debe tener exactamente 10 números y comenzar con 0.");
+            }
+            else if (txtTelefono.Text[0] != '0')
+            {
+                errorProvider1.SetError(txtTelefono, "El teléfono debe comenzar con 0.");
+            }
+            else
+            {
+                errorProvider1.SetError(txtTelefono, "");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dgvResultados.SelectedRows.Count == 1)
+            {
+                DataGridViewRow row = dgvResultados.SelectedRows[0];
+
+                // Llenar campos de texto con los datos de la fila seleccionada
+                txtCedula.Text = row.Cells[0].Value.ToString();
+                txtNombre.Text = row.Cells[1].Value.ToString();
+                txtApellido.Text = row.Cells[2].Value.ToString();
+                txtTelefono.Text = row.Cells[3].Value.ToString();
+                dtpFecha.Value = DateTime.Parse(row.Cells[4].Value.ToString());
+                txtCorreo.Text = row.Cells[5].Value.ToString();
+
+                // Eliminar la fila seleccionada del DataGridView
+                dgvResultados.Rows.Remove(row);
+            }
+            else
+            {
+                MessageBox.Show("Seleccione una fila para editar.");
+            }
+
+        }
+
+        private void txtCorreo_TextChanged(object sender, EventArgs e)
+        {
+            // Filtrar: solo permitir minúsculas, números y caracteres válidos en correos
+            string textoFiltrado = new string(txtCorreo.Text
+                .Where(c => char.IsLower(c) || char.IsDigit(c) || c == '@' || c == '.' || c == '-' || c == '_')
+                .ToArray());
+
+            // Si el texto cambió, actualizar el contenido
+            if (txtCorreo.Text != textoFiltrado)
+            {
+                int pos = txtCorreo.SelectionStart;
+                txtCorreo.Text = textoFiltrado;
+                txtCorreo.SelectionStart = Math.Min(txtCorreo.Text.Length, pos);
+            }
+        }
+    }
+}
